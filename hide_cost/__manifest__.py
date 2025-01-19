6c7bc5dadd50965c5114d9aa00fc1a7b756d2964
New Programmer <newprogrammerjava@gmail.com>
2025-01-19 18:41:50 +0700

    hide cost sales

---
+++ hide_cost/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hide Cost',
+    "version": "18.0.1.0.0",
+    'summary': 'Hide Cost ',
+    'description': """
+        This module Hide Cost for sales
+    """,
+    'author': 'Rizky Abdi Syahputra Hasibuan',
+    'website': '',
+    'category': 'Project',
+    'license': 'LGPL-3',
+    'depends': ['project'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
