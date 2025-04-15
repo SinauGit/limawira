ba6eefe1025b059fdf5957ccf2c3334a7a68e0ad
SinauGit <newprogrammerjava@gmail.com>
2025-04-15 14:20:31 +0700

    nambah stock card, tambah hak accounting

---
+++ hak_accounting/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Hak Akses Accounting LWW',
+    'version': '1.0',
+    'summary': 'Kustomisasi hak akses accounting untuk LWW',
+    'depends': ['account'],
+    'data': [
+        # 'security/res_groups.xml',
+        # 'security/ir.model.access.csv',
+        # 'views/menu_items.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
--- ship_via/models/ship_via.py
+++ ship_via/models/ship_via.py
@@ -6,6 +6,8 @@ class PurchaseOrder(models.Model):
     ship_via = fields.Selection([
         ('1', 'Lokal: Diantar'),
         ('2', 'Lokal: Diambil'),
+        ('3', 'Lokal: Air'),
+        ('4', 'Lokal: Sea'),
     ], string='Ship Via')
 
 
