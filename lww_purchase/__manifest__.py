e1d6386fc311e5bd925c2402f3612cb37d46c6c1
SinauGit <newprogrammerjava@gmail.com>
2025-05-14 10:58:54 +0700

    tambah ulang modul lww purchase

---
+++ lww_purchase/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Limawira Purchase Order",
+    'summary': '',
+    'author': '',
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['base', 'purchase'],
+    'data': [
+        # PURCHASE ORDER
+        'report/lww_po_report.xml',
+        'views/lww_po_template.xml',
+        'views/lww_po_jasa_template.xml',
+        'views/bs_po_template.xml',
+        'views/spartadua_po_template.xml',
+        'views/pratama_po_template.xml',
+        'views/imadea_po_template.xml',
+        'views/caritas_po_template.xml',
+        'views/sparindo_po_template.xml',
+        'views/wastu_po_template.xml',
+        'views/amanera_po_template.xml',
+        # FORM VIEW
+        'views/purchase_order_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
+
