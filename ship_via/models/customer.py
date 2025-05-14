789b061e9157b620cfc6b93f9c70abf393263ca6
SinauGit <newprogrammerjava@gmail.com>
2025-05-14 15:37:26 +0700

    validasi error wajib isi customer jika klik ok po jasa

--- ship_via/models/customer.py
+++ ship_via/models/customer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -15,3 +16,15 @@ class PurchaseOrder(models.Model):
         domain="[('customer_rank', '>', 0)]",  # opsional: hanya tampilkan partner bertipe customer
         help="Customer terkait dengan purchase order ini (tidak sama dengan supplier/vendor).",
     )
+    
+    @api.constrains('is_po_service', 'customer_id')
+    def _check_customer_required(self):
+        for record in self:
+            if record.is_po_service and not record.customer_id:
+                raise ValidationError(_("Customer harus diisi ketika PO Service diaktifkan."))
+                
+    @api.onchange('is_po_service') 
+    def _onchange_is_po_service(self):
+        """Reset customer_id when is_po_service is turned off"""
+        if not self.is_po_service:
+            self.customer_id = False
