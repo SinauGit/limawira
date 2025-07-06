5d5ba6f8ea2d427665fb2c599ae3c69b95696de1
SinauGit <newprogrammerjava@gmail.com>
2025-07-06 18:32:18 +0700

    tambah security access nya

--- revaluation_parsial/__manifest__.py
+++ revaluation_parsial/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '1.0',
     'depends': ['stock_account','stock'],
     'data': [
+        'security/ir.model.access.csv'
         # 'views/stock_valuation_layer_views.xml',
         # 'wizard/stock_valuation_layer_revaluation_views.xml',
     ],
