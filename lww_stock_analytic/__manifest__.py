2cdfb2bea94358d1df36d15c46b1ad2c444b09a1
SinauGit <newprogrammerjava@gmail.com>
2025-07-31 13:49:55 +0700

    tambah modul stock analytic dan lww stock analytic

---
+++ lww_stock_analytic/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Stock_Analytic",
+    "version": "18.0.1.0.0",
+    "depends": [
+        "stock_analytic",
+        # "base_view_inheritance_extension",
+    ],
+    "data": [
+        "views/stock_picking.xml",
+    ],
+}
